3732030a4c482fa34700ba4a310ee278629425fd
Kay Z <l04m33@gmail.com>
2023-06-01 00:24:21 +0800

    Update TIC-80 version and use default cmake build steps instead

--- guix-local-packages/tic-80.scm
+++ guix-local-packages/tic-80.scm
@@ -20,36 +20,40 @@
 (define tic-80
   (package
    (name "tic-80")
-   (version "v1.0.2164")
+   (version "v1.1.0-dev-20230601")
    (source (origin
             (method git-fetch)
             (uri (git-reference
                   (url "https://github.com/nesbox/TIC-80.git")
-                  (commit version)
+                  (commit "5dd72cf0")
                   (recursive? #t)))
             (file-name (git-file-name "tic-80" version))
             (sha256
              (base32
-              "0xr1i2yv85whb4c9484sgvnsx3mx4zvwkliljx55qvi8ss2q0xja"))))
+              "1xr1i2yv85whb4c9484sgvnsx3mx4zvwkliljx55qvi8ss2q0xja"))))
    (build-system cmake-build-system)
-   (arguments `(#:tests? #f
-                #:phases (modify-phases %standard-phases
-                           (replace 'configure
-                             (lambda _
-                               (let* ((tmpdir (getenv "TMPDIR"))
-                                      (build-srcdir (string-append tmpdir "/source"))
-                                      (build-dir (string-append build-srcdir "/build"))
-                                      (outdir (assoc-ref %outputs "out")))
-                                 (chdir build-dir)
-                                 (invoke "cmake"
-                                         ".."
-                                         "-DCMAKE_BUILD_TYPE=Release"
-                                         (string-append "-DCMAKE_INSTALL_PREFIX=" outdir)
-                                         "-DCMAKE_INSTALL_LIBDIR=lib"
-                                         "-DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE"
-                                         (string-append "-DCMAKE_INSTALL_RPATH=" outdir "/lib")
-                                         "-DCMAKE_VERBOSE_MAKEFILE=ON"
-                                         "-DBUILD_PRO=On")))))))
+   (arguments `(#:build-type "Release"
+                #:configure-flags
+                ("-DBUILD_WITH_JANET=TRUE"
+                 "-DBUILD_PRO=ON")))
+   ;; (arguments `(#:tests? #f
+   ;;              #:phases (modify-phases %standard-phases
+   ;;                         (replace 'configure
+   ;;                           (lambda _
+   ;;                             (let* ((tmpdir (getenv "TMPDIR"))
+   ;;                                    (build-srcdir (string-append tmpdir "/source"))
+   ;;                                    (build-dir (string-append build-srcdir "/build"))
+   ;;                                    (outdir (assoc-ref %outputs "out")))
+   ;;                               (chdir build-dir)
+   ;;                               (invoke "cmake"
+   ;;                                       ".."
+   ;;                                       "-DCMAKE_BUILD_TYPE=Release"
+   ;;                                       (string-append "-DCMAKE_INSTALL_PREFIX=" outdir)
+   ;;                                       "-DCMAKE_INSTALL_LIBDIR=lib"
+   ;;                                       "-DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE"
+   ;;                                       (string-append "-DCMAKE_INSTALL_RPATH=" outdir "/lib")
+   ;;                                       "-DCMAKE_VERBOSE_MAKEFILE=ON"
+   ;;                                       "-DBUILD_PRO=On")))))))
    (inputs (list pkg-config
                  ruby
                  python-wrapper
