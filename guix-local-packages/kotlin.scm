444f1df66fc3ed1ba5948a48a1fcfa10ef21720f
Kay Z <l04m33@gmail.com>
2023-06-14 10:50:22 +0800

    kotlin-language-server

--- guix-local-packages/kotlin.scm
+++ guix-local-packages/kotlin.scm
@@ -7,6 +7,7 @@
   #:use-module (gnu packages base)
   #:use-module (gnu packages java))
 
+
 (define-public kotlin-compiler
   (package
     (name "kotlin-compiler")
@@ -31,3 +32,26 @@
     (synopsis "Kotlin compiler for the command line")
     (description "Kotlin compiler for the command line")
     (license license:expat))) ;; XXX: the source code used multiple licenses
+
+
+(define-public kotlin-language-server
+  (package
+    (name "kotlin-language-server")
+    (version "1.3.3")
+    (source
+     (origin
+      (method url-fetch/zipbomb)
+      (uri (string-append "https://github.com/fwcd/kotlin-language-server/releases/download/"
+                          version
+                          "/server.zip"))
+      (sha256
+       (base32 "1q0z13q7q2s98bhvbdnn44n1wq5aszr7ra436vbz2d6c9spinncr"))))
+    (build-system binary-build-system)
+    (arguments
+     `(#:install-plan `(("./server" "."))))
+    (propagated-inputs
+     `(("kotlin-compiler" ,kotlin-compiler)))
+    (home-page "https://github.com/fwcd/kotlin-language-server")
+    (synopsis "Language server for the Kotlin language")
+    (description "Language server for the Kotlin language")
+    (license license:expat))) ;; MIT
